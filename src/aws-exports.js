6da7003120857ddf276faf3e2060504bfdd06f7e
Admral Walker <admralwalker@Mac.lan>
2025-11-16 14:41:01 -0800

    SAAL: Fix Vite base, SPA redirects, aws-exports

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,11 +1,9 @@
-// src/aws-exports.js
 const awsconfig = {
-  aws_project_region: "us-west-2",
-  aws_cognito_region: "us-west-2",
-  aws_user_pools_id: "us-west-2_GW0n4A2xc",
-  aws_user_pools_web_client_id: "23ns3k2juhn130jdfkseritqpm",
-  oauth: {},
+    aws_project_region: "us-west-2",
+    aws_cognito_region: "us-west-2",
+    aws_user_pools_id: "us-west-2_bMLXffkHg",
+    aws_user_pools_web_client_id: "XXXXXXXXXXXXXXXXXXXX",
+    oauth: {},
+    aws_cloud_logic_custom: [],
 };
-
 export default awsconfig;
-
