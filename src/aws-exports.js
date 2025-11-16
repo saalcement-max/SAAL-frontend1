1975537d02abadf8aaa536f807a08e06717aa386
Admral Walker <admralwalker@Mac.lan>
2025-11-16 15:19:01 -0800

    Update aws-exports.js with correct Cognito pool and client

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,9 +1,15 @@
-const awsconfig = {
+// src/aws-exports.js
+const awsmobile = {
     aws_project_region: "us-west-2",
+    aws_cognito_identity_pool_id: "", // Optional if not using Identity Pool
     aws_cognito_region: "us-west-2",
-    aws_user_pools_id: "us-west-2_bMLXffkHg",
-    aws_user_pools_web_client_id: "XXXXXXXXXXXXXXXXXXXX",
+    aws_user_pools_id: "us-west-2_atOwvbO12",
+    aws_user_pools_web_client_id: "4vq96f8njdfqgr2qaq75hokuak",
     oauth: {},
-    aws_cloud_logic_custom: [],
+    aws_appsync_graphqlEndpoint: "https://<YOUR_APPSYNC_API_ID>.appsync-api.us-west-2.amazonaws.com/graphql", // Replace if using AppSync
+    aws_appsync_region: "us-west-2",
+    aws_appsync_authenticationType: "AMAZON_COGNITO_USER_POOLS",
 };
-export default awsconfig;
+
+export default awsmobile;
+
