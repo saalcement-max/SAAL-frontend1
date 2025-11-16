bcd1b97ec03c2b007e418990a06d24f02d8c1e24
Admral Walker <admralwalker@Mac.lan>
2025-11-16 14:32:12 -0800

    SAAL: Fix Vite output, SPA redirects, and domain redirect

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
   },
   server: {
     port: 5173,
-  }
+  },
 });
