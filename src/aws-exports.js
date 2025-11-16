61e7c71dacab5fc2c6820264d7602fe6f1e5b267
Admral Walker <admralwalker@Mac.lan>
2025-11-16 15:47:00 -0800

    Update Cognito pool and frontend app client

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,15 +1,13 @@
-// src/aws-exports.js
-const awsmobile = {
-    aws_project_region: "us-west-2",
-    aws_cognito_identity_pool_id: "", // Optional if not using Identity Pool
-    aws_cognito_region: "us-west-2",
-    aws_user_pools_id: "us-west-2_atOwvbO12",
-    aws_user_pools_web_client_id: "4vq96f8njdfqgr2qaq75hokuak",
-    oauth: {},
-    aws_appsync_graphqlEndpoint: "https://<YOUR_APPSYNC_API_ID>.appsync-api.us-west-2.amazonaws.com/graphql", // Replace if using AppSync
-    aws_appsync_region: "us-west-2",
-    aws_appsync_authenticationType: "AMAZON_COGNITO_USER_POOLS",
+cat > src/aws-exports.js << 'EOF'
+const awsExports = {
+  aws_project_region: "us-west-2",
+  aws_cognito_identity_pool_id: "",
+  aws_cognito_region: "us-west-2",
+  aws_user_pools_id: "us-west-2_atOwvbO12",
+  aws_user_pools_web_client_id: "4vq96f8njdfqgr2qaq75hokuak",
+  oauth: {},
 };
 
-export default awsmobile;
+export default awsExports;
+EOF
 
