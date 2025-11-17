1ae88f293068de0cc8ba735d2e9922defe783102
Admral Walker <admralwalker@Mac.lan>
2025-11-17 13:52:12 -0800

    chore: update aws-exports (Cognito + region)

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,13 +1,17 @@
-cat > src/aws-exports.js << 'EOF'
-const awsExports = {
+// src/aws-exports.js
+const awsconfig = {
   aws_project_region: "us-west-2",
-  aws_cognito_identity_pool_id: "",
   aws_cognito_region: "us-west-2",
   aws_user_pools_id: "us-west-2_atOwvbO12",
   aws_user_pools_web_client_id: "4vq96f8njdfqgr2qaq75hokuak",
-  oauth: {},
+  Auth: {
+    region: "us-west-2",
+    userPoolId: "us-west-2_atOwvbO12",
+    userPoolWebClientId: "4vq96f8njdfqgr2qaq75hokuak"
+  },
+  aws_content_delivery_bucket: "",        // optional
+  aws_content_delivery_bucket_region: ""  // optional
 };
 
-export default awsExports;
-EOF
+export default awsconfig;
 
